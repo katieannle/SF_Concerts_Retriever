22da8e374198aa985555f3a76c68a064119c31f2
Katie Le <katie_le@symantec.com>
2015-03-26 10:25:01 -0700

    added stylesheets in public folder

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var app = express();
 app.set('view engine', 'html');     // Treat HTML files as dynamic
 app.engine('html', hbs.__express);  // Load templating engine
 app.use(require('./routes'));
+app.use(express.static('public'));
 
 app.listen('8081');
 console.log('Listening on port 8081');
